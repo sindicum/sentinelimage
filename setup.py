a290d0446863fc394d89e8a242dd3ff9edd42f42
M. Goto <ohkenmb2@ohkenMB2.local>
2022-12-18 10:44:54 +0900

    micro fix

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='sentinelimage',
-    version='0.16',
+    version='0.17',
     packages=find_packages(),
     install_requires=[
         'earthengine-api>=0.1.326',
