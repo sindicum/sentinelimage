a6c085d5103e381dbdfee134dd68f745e48b55ee
M. Goto <ohkenxii@ohkenxii.local>
2025-02-05 19:00:55 +0900

    fix geetools dependency

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name='sentinelimage',
-    version='0.18',
+    version='0.19',
     packages=find_packages(),
     install_requires=[
         'earthengine-api>=0.1.326',
-        'geetools>=0.6.14'
+        'geetools=0.6.14'
         ]
 )
